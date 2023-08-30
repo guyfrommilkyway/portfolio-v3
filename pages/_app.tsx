35aa4ca87d58efd5d29dfc6a2eb98cf155108a55
Almer Tampus <almerflorestampus@gmail.com>
2023-08-30 16:44:08 +0800

    Organize import statements

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,8 @@
-import '@/styles/globals.css'
+// import packages below
 import type { AppProps } from 'next/app'
+
+// import assets below
+import '@/styles/globals.scss'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
