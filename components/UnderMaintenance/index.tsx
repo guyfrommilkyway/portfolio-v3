df1db0f8516241b8bad717816438dfb5a40ee7d7
Almer Tampus <almerflorestampus@gmail.com>
2023-08-30 16:43:55 +0800

    Add UnderMaintenance component

---
+++ components/UnderMaintenance/index.tsx
@@ -0,0 +1,17 @@
+// import packages below
+import Main from './components/Main'
+import Header from './components/Header'
+import Content from './components/Content'
+import Footer from './components/Footer'
+
+const UnderMaintenance = () => {
+  return (
+    <Main>
+      <Header />
+      <Content />
+      <Footer />
+    </Main>
+  )
+}
+
+export default UnderMaintenance;
