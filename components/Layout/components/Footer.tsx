3aa20f030624f0b79e17af18ae108afb0c469ff4
Almer Tampus <almerflorestampus@gmail.com>
2023-09-14 19:58:04 +0800

    Add Footer component

---
+++ components/Layout/components/Footer.tsx
@@ -0,0 +1,12 @@
+// import packages below
+import React from 'react';
+
+const Footer: React.FC = () => {
+  return (
+    <div>
+      <h1>Footer</h1>
+    </div>
+  );
+};
+
+export default Footer;
