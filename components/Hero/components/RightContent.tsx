9739dc37079738f6a1a1e39a054a7907460a08a5
Almer Tampus <almerflorestampus@gmail.com>
2023-09-19 23:51:35 +0800

    Add RightContent component

---
+++ components/Hero/components/RightContent.tsx
@@ -0,0 +1,30 @@
+// packages below
+import React from 'react';
+
+const RightContent: React.FC = () => {
+  return (
+    <div className='w-full'>
+      <p className='mb-4 text-md'>
+        Donec ut tortor consequat, pellentesque enim id, suscipit libero. Cras vel nunc tincidunt,
+        vulputate risus at, tincidunt orci. Sed commodo sapien eu eros mattis commodo. Ut pharetra
+        at est vitae ultrices. Proin vulputate congue sodales. Nullam tristique imperdiet arcu, quis
+        varius lorem varius et. In porta condimentum elit, non facilisis mi pulvinar vitae. Ut
+        ligula erat, ultricies et nibh ac, vulputate mattis massa. Proin dui massa, vestibulum et
+        ligula id, ornare accumsan neque.
+      </p>
+      <p className='mb-4 text-md'>
+        Maecenas nisi sem, facilisis non convallis et, iaculis ac ex. Ut interdum lorem vitae augue
+        malesuada pulvinar. Morbi a arcu consectetur, feugiat orci vitae, imperdiet purus. Vivamus
+        maximus lacus et placerat blandit. Nam id neque at massa molestie feugiat. Nulla pretium
+        ultricies augue, eu consectetur nisl varius ac. Ut elementum purus odio, quis molestie mi
+        facilisis ac. Praesent sollicitudin ex metus, a auctor arcu porttitor non.
+      </p>
+      <p className='mb-4 text-md'>
+        Phasellus sodales, risus ut laoreet maximus, orci sem blandit ex, eget luctus dui lacus a
+        massa. Etiam et ex augue. Donec eget leo ullamcorper leo mollis sodales.
+      </p>
+    </div>
+  );
+};
+
+export default RightContent;
