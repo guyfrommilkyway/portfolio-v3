cec19b3b646ff9eea603d49ab59e62eaf5bb7478
Almer Tampus <almerflorestampus@gmail.com>
2023-09-25 20:45:27 +0800

    Minor UI update

--- components/Experience/components/Card.tsx
+++ components/Experience/components/Card.tsx
@@ -15,14 +15,14 @@ const Card: React.FC<CardProps> = (props) => {
 
 	return (
 		<div className='flex flex-col lg:flex-row gap-4 w-full p-4 tracking-wide leading-relaxed rounded-sm transition-transform ease-in-out delay-150 hover:scale-105 hover:bg-neutral-100'>
-			<div className='lg:w-[20%]'>
+			<div className='w-full lg:w-[20%]'>
 				<span className='w-fit text-neutral-500 text-sm uppercase leading-none tracking-tighter '>
 					{duration}
 				</span>
 			</div>
-			<div className='w-[80%]'>
-				<h6 className='mb-2 text-neutral-700'>{title}</h6>
-				<h6 className='mb-2 font-semibold'>{company}</h6>
+			<div className='w-full lg:w-[80%]'>
+				<h6 className='mb-2 font-semibold'>{title}</h6>
+				<h6 className='mb-2text-neutral-700'>{company}</h6>
 				<a className='flex gap-2 w-fit mb-2 underline text-neutral-700' href={link} target='_blank'>
 					{website}
 					<ArrowUpSVG width={14} height={14} />
