c660029a78f05771a815de87a09224915f7a934b
Almer Tampus <almerflorestampus@gmail.com>
2023-09-25 20:26:07 +0800

    Remove unnecessary components

--- pages/develop/index.tsx
+++ pages/develop/index.tsx
@@ -3,13 +3,20 @@ import { Fragment } from 'react';
 
 // import components below
 import Head from '@/components/Head';
-import Hero from '@/components/Hero';
+import Layout from '@/components/Layout';
+import About from '@/components/About';
+import Experience from '@/components/Experience';
+import Projects from '@/components/Projects';
 
 export default function Home() {
-  return (
-    <Fragment>
-      <Head title='Almer Tampus' />
-      <Hero />
-    </Fragment>
-  );
+	return (
+		<Fragment>
+			<Head title='Almer Tampus' />
+			<Layout>
+				<About />
+				<Experience />
+				<Projects />
+			</Layout>
+		</Fragment>
+	);
 }
