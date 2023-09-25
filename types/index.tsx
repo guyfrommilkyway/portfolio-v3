8bab6411f14695234cad46a93354e8ba99db0427
Almer Tampus <almerflorestampus@gmail.com>
2023-09-25 20:26:16 +0800

    Update types

--- types/index.tsx
+++ types/index.tsx
@@ -2,5 +2,33 @@
 import { ReactNode } from 'react';
 
 export interface ChildrenProps {
-  children: ReactNode;
+	children: ReactNode;
+}
+
+export interface NavItemProps {
+	name: string;
+	onScroll: () => void;
+}
+
+export interface CardProps {
+	duration: string;
+	title: string;
+	company: string;
+	link: string;
+	website: string;
+	description: string;
+	technologies: string[];
+}
+
+export interface PillProps {
+	tech: string;
+}
+
+export interface WorkCardProps {
+	name: string;
+	link: string;
+	website: string;
+	description: string;
+	technologies: string[];
+	image: string;
 }
