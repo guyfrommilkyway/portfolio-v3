7799dd1be200dc6c17623b649460adb2741d9020
Almer Tampus <almerflorestampus@gmail.com>
2023-09-25 22:00:38 +0800

    Add data; Enable SSG

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,26 @@ import About from '@/components/About';
 import Experience from '@/components/Experience';
 import Projects from '@/components/Projects';
 
+// data below
+import { experience } from '@/data/experience';
+import { work } from '@/data/work';
+import { personal } from '@/data/personal';
+
 export default function Home() {
 	return (
 		<Fragment>
 			<Head title='Almer Tampus' />
 			<Layout>
 				<About />
-				<Experience />
-				<Projects />
+				<Experience data={experience} />
+				<Projects data={work} data2={personal} />
 			</Layout>
 		</Fragment>
 	);
+}
+
+export async function getStaticProps() {
+	return {
+		props: {},
+	};
 }
