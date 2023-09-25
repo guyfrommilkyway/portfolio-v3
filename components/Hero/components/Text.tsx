3490119d2eeff284f18054af68aa6cde31858422
Almer Tampus <almerflorestampus@gmail.com>
2023-09-26 04:19:21 +0800

    Add data

--- components/Hero/components/Text.tsx
+++ components/Hero/components/Text.tsx
@@ -12,8 +12,10 @@ const Text: React.FC = () => {
 		<Fragment>
 			<h1 className='mb-2 font-bold text-4xl leading-tight tracking-wide'>Almer Tampus</h1>
 			<p className='mb-4 text-xl text-neutral-700'>Agile Web Developer</p>
-			<p className='w-full mb-8 text-lg text-neutral-700 leading-relaxed'>
-				Elevating the Digital Experience on the Web
+			<p className='w-full max-w-[540px] mb-8 text-lg text-neutral-700 leading-relaxed'>
+				Deeply committed to infusing innovation and user-centricity into every project I take on.
+				Continuously strive to craft captivating and seamless online experiences that leave a
+				lasting impact on users.
 			</p>
 			<div className='flex gap-4'>
 				<a
@@ -21,30 +23,35 @@ const Text: React.FC = () => {
 					href='https://linkedin.com/in/almerflorestampus'
 					target='_blank'
 				>
-					<LinkedInSVG width='28px' height='28px' />
+					<LinkedInSVG width='32px' height='32px' />
 				</a>
 				<a
 					className='transition ease-in-out delay-100 hover:scale-125'
 					href='https://github.com/guyfrommilkyway'
 					target='_blank'
 				>
-					<GithubSVG width='28px' height='28px' />
+					<GithubSVG width='32px' height='32px' />
 				</a>
 				<a
 					className='transition ease-in-out delay-100 hover:scale-125'
 					href='https://twitter.com/guyfrommilkyway'
 					target='_blank'
 				>
-					<TwitterSVG width='28px' height='28px' />
+					<TwitterSVG width='32px' height='32px' />
 				</a>
 				<a
 					className='transition ease-in-out delay-100 hover:scale-125'
 					href='https://instagram.com/aftampus'
 					target='_blank'
 				>
-					<InstagramSVG width='28px' height='28px' />
+					<InstagramSVG width='32px' height='32px' />
 				</a>
 			</div>
+			{/* <div className='flex gap-8 lg:gap-16 items-center'>
+				<button className='w-fit px-6 py-3 text-white uppercase hover:underline bg-black rounded-sm transition-transform ease-in-out delay-100 hover:scale-105'>
+					Hire Me
+				</button>
+			</div> */}
 		</Fragment>
 	);
 };
--- data/hero.ts
+++ data/hero.ts
@@ -2,7 +2,11 @@
 import { Hero } from '@/types';
 
 export const hero: Hero = {
-	headline: '',
-	paragraphs: [],
+	headline: 'Born in 1997. Raised in a small town in Philippines.',
+	paragraphs: [
+		'Almer is a true self-starter coder (and aspiring designer). He has over a year of professional experience, specializing in front-end React development. Despite still being early in his career, he has already made significant contribution to various projects. In his most recent position at Seaplane Group, a Hong Kong-based startup, he took on the responsibility of maintaining and developing user-facing features for two company projects: the Mile High Roller Club and Operator Portal.',
+		"He's also known for being passionate and dedicated to continuous learning. To stay at the forefront of the ever-evolving tech landscape, he has dedicated significant time and effort to investing in his personal growth and development through a diverse range of learning avenues, including comprehensive online courses and rigorous self-study sessions. His main focus in these days has been on broadening his existing skill set and expertise into adjacent domains, encompassing project management, user experience design, backend development, and the intricacies of DevOps.",
+		"When he's not at his workstation, you'll often find him engaged in spending quality time with his loyal German Shepherd, Comet, forging a bond while honing their skills together. When not in training mode, his other canine companions, Brew and Latte, eargerly join the mix, creating a playful and vibrant atmosphere.",
+	],
 	technologies: [],
 };
