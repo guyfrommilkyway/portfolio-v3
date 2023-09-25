5244de2c2a7245d19e7755ffbfcd5d504398cc66
Almer Tampus <almerflorestampus@gmail.com>
2023-09-25 20:29:23 +0800

    Add Logo component

---
+++ components/Logo/index.tsx
@@ -0,0 +1,12 @@
+// packages belo
+import React from 'react';
+
+const Logo: React.FC = () => {
+	return (
+		<div className=''>
+			<span className='font-bold'>AFT</span>
+		</div>
+	);
+};
+
+export default Logo;
