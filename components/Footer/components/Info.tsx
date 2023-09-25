75bb599c8a23ef36971df8ac7922ebdf5cfae130
Almer Tampus <almerflorestampus@gmail.com>
2023-09-26 05:50:04 +0800

    Update from Netlify to Vercel

--- components/Footer/components/Info.tsx
+++ components/Footer/components/Info.tsx
@@ -2,7 +2,7 @@ const Info = () => {
 	return (
 		<div className='block text-xs text-center md:text-right text-neutral-700'>
 			<p className=''>Built using Next.js and Tailwind.</p>
-			<p className=''>Made from Figma. Deployed with Netlify.</p>
+			<p className=''>Made from Figma. Deployed with Vercel.</p>
 		</div>
 	);
 };
