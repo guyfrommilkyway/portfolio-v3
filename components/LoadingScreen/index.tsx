40454b5c0dc8d3d754eee4fed15b85386f9ad31a
Almer Tampus <almerflorestampus@gmail.com>
2023-09-27 12:01:47 +0800

    Minor UI update

--- components/LoadingScreen/index.tsx
+++ components/LoadingScreen/index.tsx
@@ -15,13 +15,9 @@ const LoadingScreen: React.FC<LoadingScreenProps> = (props) => {
 
 	return (
 		<div
-			className={`fixed top-0 right-0 bottom-0 left-0  w-screen h-screen bg-neutral-600 transition-opacity ease-in-out delay-200 ${loading} ${removing}`}
+			className={`fixed top-0 right-0 bottom-0 left-0 z-[99] flex justify-center items-center w-screen h-screen bg-neutral-600 transition-opacity ease-in-out delay-200 ${loading} ${removing}`}
 		>
-			<SpinnerSVG
-				className='absolute top-[50%] right-[50%] translate-x[-50%] translate-y-[-50%]'
-				width={80}
-				height={80}
-			/>
+			<SpinnerSVG width={80} height={80} />
 		</div>
 	);
 };
