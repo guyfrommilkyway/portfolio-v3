db2a88bc2e4d9a6a6d59659007d22f93f1899c67
Almer Tampus <almerflorestampus@gmail.com>
2023-10-09 20:25:57 +0800

    Wrap section components with ContentBox component

--- components/Projects/index.tsx
+++ components/Projects/index.tsx
@@ -1,17 +1,18 @@
-// packages below
+// packages
 import React from 'react';
 
-// components below
+// components
+import ContentBox from '@/components/ContentBox';
 import Card from './components/Card';
 
-// types below
+// types
 import { ProjectsProps } from '@/types';
 
 const Projects: React.FC<ProjectsProps> = (props) => {
 	const { data, data2 } = props;
 
 	return (
-		<section className='project relative w-full px-4 py-16 md:p-16 lg:px-20 border-b border-neutral-900'>
+		<ContentBox id='project'>
 			<div className='mb-20'>
 				<h3 className='mb-8 text-white text-2xl font-semibold'>Work</h3>
 				<div className='flex flex-col gap-8'>
@@ -28,7 +29,7 @@ const Projects: React.FC<ProjectsProps> = (props) => {
 					})}
 				</div>
 			</div>
-		</section>
+		</ContentBox>
 	);
 };
 
