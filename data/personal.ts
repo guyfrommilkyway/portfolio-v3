2191ffaf5766ea8e9133260303773ee712515aae
Almer Tampus <almerflorestampus@gmail.com>
2023-10-09 23:02:07 +0800

    Update data

--- data/personal.ts
+++ data/personal.ts
@@ -6,8 +6,7 @@ export const personal: ProjectItem[] = [
 		name: 'React Notes',
 		link: 'https://react-notes.almertampus.xyz/',
 		website: 'react-notes.almertampus.xyz',
-		description:
-			"React Notes is an application designed and developed to make note-taking easier. It's inspired by Google Keep Note",
+		description: 'Inspired by Google Keep Note',
 		technologies: [
 			'TypeScript',
 			'React',
@@ -23,40 +22,15 @@ export const personal: ProjectItem[] = [
 		name: 'Portfolio V3',
 		link: 'https://github.com/guyfrommilkyway/portfolio-v3/',
 		website: 'github.com/guyfrommilkyway/portfolio-v3',
-		description: 'Portfolio V3 website by yours truly.',
+		description: 'Portfolio website by yours truly',
 		technologies: ['TypeScript', 'React', 'Next.js', 'Context API', 'Tailwind'],
 		image: '/images/portfolio-v3.png',
 	},
-	// {
-	// 	name: 'Drum Machine',
-	// 	link: 'https://drum-machine.almertampus.xyz',
-	// 	website: 'drum-machine.almertampus.xyz',
-	// 	description: 'A simple drum machine application built using JavaScript and React.',
-	// 	technologies: ['JavaScript', 'React', 'Next.js', 'Chakra UI'],
-	// 	image: '/images/drum-machine.png',
-	// },
-	// {
-	// 	name: 'JavaScript Calculator',
-	// 	link: 'https://js-calculator.almertampus.xyz',
-	// 	website: 'js-calculator.almertampus.xyz',
-	// 	description: 'A simple calculator application built using JavaScript and React.',
-	// 	technologies: ['JavaScript', 'React', 'Next.js', 'Context API', 'Tailwind'],
-	// 	image: '/images/js-calculator.png',
-	// },
-	// {
-	// 	name: '25+5 Clock',
-	// 	link: 'https://25-5-clock.almertampus.xyz',
-	// 	website: '25-5-clock.almertampus.xyz',
-	// 	description: 'A simple clock application built using JavaScript and React.',
-	// 	technologies: ['JavaScript', 'React', 'Next.js', 'Redux', 'Redux Toolkit', 'React Bootstrap'],
-	// 	image: '/images/25-5-clock.png',
-	// },
 	{
-		name: 'dms v1',
+		name: 'DMS',
 		link: 'https://github.com/guyfrommilkyway/dms-v1',
 		website: 'github.com/guyfrommilkyway/dms-v1',
-		description:
-			"Dormitory Management System or DMS is an application designed and developed to help dormitory owners and management manager their dormitories's day-to-day activities.",
+		description: 'Dormitory Management System',
 		technologies: [
 			'JavaScript',
 			'Node.js',
