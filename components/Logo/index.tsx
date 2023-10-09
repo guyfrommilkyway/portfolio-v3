9649b9156eb69310613230092cd199757ee68d6c
Almer Tampus <almerflorestampus@gmail.com>
2023-10-09 14:01:17 +0800

    Add text next to Logo component

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -3,9 +3,15 @@ import React from 'react';
 
 // assets
 import LogoSVG from '@/assets/svg/logo.svg';
+import Link from 'next/link';
 
 const Logo: React.FC = () => {
-	return <LogoSVG width='42px' />;
+	return (
+		<Link href='/' className='flex items-center gap-2'>
+			<LogoSVG width='42px' />
+			<span className='text-white hover:underline'>AFT Studio</span>
+		</Link>
+	);
 };
 
 export default Logo;
