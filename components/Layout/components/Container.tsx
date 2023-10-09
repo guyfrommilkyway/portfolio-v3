883112718971bcaf46ec6b416c5b635f32d0bf53
Almer Tampus <almerflorestampus@gmail.com>
2023-10-09 23:30:25 +0800

    Remove interface delay

--- components/Layout/components/Container.tsx
+++ components/Layout/components/Container.tsx
@@ -4,17 +4,11 @@ import React, { useState, useEffect } from 'react';
 // types
 import { ChildrenProps } from '@/types';
 
-interface Props extends ChildrenProps {
-	loading: string;
-}
-
-const Container: React.FC<Props> = (props) => {
-	const { loading, children } = props;
+const Container: React.FC<ChildrenProps> = (props) => {
+	const { children } = props;
 
 	return (
-		<main
-			className={`relative flex flex-col lg:flex-row gap-0 w-full max-w-[1920px] h-full mx-auto border-x border-neutral-900 transition-opacity ease-in-out delay-150 ${loading}`}
-		>
+		<main className='relative flex flex-col lg:flex-row gap-0 w-full max-w-[1920px] h-full mx-auto border-x border-neutral-900 transition-opacity ease-in-out delay-150'>
 			{children}
 		</main>
 	);
