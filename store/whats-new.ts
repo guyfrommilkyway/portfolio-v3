39c79893b12a5120560492f6c0c802ac25e9f27b
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 01:18:30 +0800

    Add useWhatsNewStore

---
+++ store/whats-new.ts
@@ -0,0 +1,17 @@
+// packages
+import { create } from 'zustand';
+import { devtools, persist } from 'zustand/middleware';
+
+const useWhatsNewStore = create<any>()(
+	devtools(
+		persist(
+			(set) => ({
+				whatsnew: {},
+				dataHandler: (data: any) => set({ whatsnew: data }),
+			}),
+			{ name: 'whatsnew-store' }
+		)
+	)
+);
+
+export default useWhatsNewStore;
