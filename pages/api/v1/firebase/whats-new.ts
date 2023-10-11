5cc341e2b37f2e6e8653ca38a900eb17319bb758
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 03:57:08 +0800

    Add /api/v1/firebase/whats-new endpoint

---
+++ pages/api/v1/firebase/whats-new.ts
@@ -0,0 +1,15 @@
+// packages
+import { NextApiRequest, NextApiResponse } from 'next';
+
+// utils
+import fetchFirebase from '@/services/firebase';
+
+export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+	try {
+		const response = await fetchFirebase('whatsnew');
+
+		res.status(200).json(response);
+	} catch (error) {
+		res.status(400);
+	}
+}
