c7e5e54f48a301ae54e77fe24ebce7d65a1eb367
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 02:27:39 +0800

    Refactor Layout component

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -4,22 +4,21 @@ import { Analytics } from '@vercel/analytics/react';
 
 // components
 import Container from './components/Container';
-import Hero from '@/components/Hero';
 import Content from './components/Content';
 import News from '@/components/News';
 
 // types
-import { ChildrenProps } from '@/types';
+import { LayoutProps } from '@/types';
 
-const Layout: React.FC<ChildrenProps> = (props) => {
-	const { children } = props;
+const Layout: React.FC<LayoutProps> = (props) => {
+	const { renderHero, renderContent, renderNews } = props;
 
 	return (
 		<Fragment>
 			<Container>
-				<Hero />
-				<Content>{children}</Content>
-				<News />
+				{renderHero()}
+				<Content>{renderContent()}</Content>
+				<News>{renderNews()}</News>
 			</Container>
 			<Analytics />
 		</Fragment>
