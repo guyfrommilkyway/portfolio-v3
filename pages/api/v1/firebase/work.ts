30a4608e03609f2458b72df186fb233f749c8725
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 04:58:53 +0800

    Set necessary CORS headers

--- pages/api/v1/firebase/work.ts
+++ pages/api/v1/firebase/work.ts
@@ -6,6 +6,11 @@ import fetchFirebase from '@/services/firebase';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
 	try {
+		// Set the necessary CORS headers
+		res.setHeader('Access-Control-Allow-Origin', 'https://almertampus.xyz');
+		res.setHeader('Access-Control-Allow-Methods', 'GET');
+		res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
 		const response = await fetchFirebase('work');
 
 		res.status(200).json(response);
