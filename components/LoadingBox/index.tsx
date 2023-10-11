a46a61d1fe00570f22830e164c9e8fd5b28d91e5
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 01:27:13 +0800

    Add LoadingBox component

---
+++ components/LoadingBox/index.tsx
@@ -0,0 +1,15 @@
+// packages
+import React from 'react';
+
+// asset
+import LoadingSVG from '@/assets/svg/rolling.svg';
+
+const LoadingBox: React.FC = (props) => {
+	return (
+		<div className='flex justify-center items-center w-full h-[80px]'>
+			<LoadingSVG width={32} height={32} />
+		</div>
+	);
+};
+
+export default LoadingBox;
