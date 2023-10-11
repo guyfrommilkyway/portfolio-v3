4d67ac945b6c7fb76cf4efadbf66245c1f4f7d16
Almer Tampus <almerflorestampus@gmail.com>
2023-10-12 02:28:47 +0800

    Refactor News component

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -1,15 +1,15 @@
 // packages
 import React from 'react';
 
-// components
-import WhatsNew from '@/components/WhatsNew';
-import RecentNews from '@/components/RecentNews';
+// types
+import { ChildrenProps } from '@/types';
 
-const News: React.FC = () => {
+const News: React.FC<ChildrenProps> = (props) => {
+	const { children } = props;
+
 	return (
 		<aside className='hidden xl:block sticky top-0 h-fit w-full max-w-[340px] pt-8 pb-36'>
-			<WhatsNew />
-			<RecentNews />
+			{children}
 		</aside>
 	);
 };
