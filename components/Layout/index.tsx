935a2345056edafd133aa30f52cfbb0d313b666d
Almer Tampus <almerflorestampus@gmail.com>
2023-10-13 07:18:22 +0800

    Relocate News child components

--- components/Layout/index.tsx
+++ components/Layout/index.tsx
@@ -6,16 +6,23 @@ import { Analytics } from '@vercel/analytics/react';
 import Container from './components/Container';
 import Content from './components/Content';
 import News from '@/components/News';
+import Resume from '@/components/Resume';
+import WhatsNew from '@/components/WhatsNew';
+import RecentNews from '@/components/RecentNews';
 
 const Layout: React.FC<LayoutProps> = (props) => {
-	const { renderHero, renderContent, renderNews } = props;
+	const { renderHero, renderContent } = props;
 
 	return (
 		<Fragment>
 			<Container>
 				{renderHero()}
 				<Content>{renderContent()}</Content>
-				<News>{renderNews()}</News>
+				<News>
+					<Resume />
+					<WhatsNew />
+					<RecentNews />
+				</News>
 			</Container>
 			<Analytics />
 		</Fragment>
