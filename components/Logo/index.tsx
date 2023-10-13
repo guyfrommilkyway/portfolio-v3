307d05eab0adc21f548268f252fa1a3eb9b86eb9
Almer Tampus <almerflorestampus@gmail.com>
2023-10-14 02:16:00 +0800

    Update logo

--- components/Logo/index.tsx
+++ components/Logo/index.tsx
@@ -1,15 +1,14 @@
 // packages
 import React from 'react';
+import Link from 'next/link';
 
 // assets
-import LogoSVG from '@/assets/svg/logo-white.svg';
-import Link from 'next/link';
+import LogoSVG from '@/assets/svg/logo.svg';
 
 const Logo: React.FC = () => {
 	return (
-		<Link href='/' className='flex items-center gap-4'>
-			<LogoSVG className='animate-pulse' width={42} height={42} />
-			<span className='text-lg text-white'>aft studio</span>
+		<Link href='/' className='flex items-center gap-3'>
+			<LogoSVG width={28} height={28} />
 		</Link>
 	);
 };
