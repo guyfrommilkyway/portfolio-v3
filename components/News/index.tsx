131d08ce9d2af62c464f4e4f6426b234598840a8
Almer Tampus <almerflorestampus@gmail.com>
2023-10-13 08:29:13 +0800

    Minor UI improvements

--- components/News/index.tsx
+++ components/News/index.tsx
@@ -5,7 +5,7 @@ const News: React.FC<ChildrenProps> = (props) => {
 	const { children } = props;
 
 	return (
-		<aside className='hidden xl:flex sticky top-0 flex-col gap-4 w-full max-w-[340px] h-fit pt-8 pb-36 px-4'>
+		<aside className='hidden xl:flex sticky top-0 flex-col gap-4 w-full max-w-[340px] h-fit pt-4 pb-36 px-4'>
 			{children}
 		</aside>
 	);
