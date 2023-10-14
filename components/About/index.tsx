8295159e566ff0389dede100ed9cd0579f3a52a1
Almer Tampus <almerflorestampus@gmail.com>
2023-10-14 17:54:24 +0800

    Minor UI enhancements

--- components/About/index.tsx
+++ components/About/index.tsx
@@ -4,44 +4,25 @@ import React from 'react';
 // components
 import ContentBox from '@/components/ContentBox';
 
-// utils
-import { toastSuccess } from '@/utils/notifications';
+const About: React.FC<AboutProps> = props => {
+  const { headline, subheadline, paragraphs } = props;
 
-const About: React.FC<AboutProps> = (props) => {
-	const { headline, subheadline, paragraphs } = props;
-
-	// copy-paste handler
-	const copyPasteHandler = () => {
-		navigator.clipboard.writeText('almerflorestampus@gmail.com');
-		toastSuccess('Copied to clipboard');
-	};
-
-	return (
-		<ContentBox id='about'>
-			<h3 className='mb-4 text-white text-2xl font-semibold'>{headline}</h3>
-			<h4 className='mb-4 text-neutral-100 font-bold text-xl tracking-wide leading-normal'>
-				{subheadline}
-			</h4>
-			{!!paragraphs &&
-				Object.keys(paragraphs).map((item) => {
-					return (
-						<p key={item} className='mb-4 leading-relaxed text-neutral-300'>
-							{paragraphs[item]}
-						</p>
-					);
-				})}
-			<p className='mb-4 leading-relaxed text-neutral-300'>
-				You can reach me at{' '}
-				<span
-					className='text-neutral-300 underline hover:text-white cursor-copy'
-					onClick={copyPasteHandler}
-				>
-					almerflorestampus@gmail.com
-				</span>
-				.
-			</p>
-		</ContentBox>
-	);
+  return (
+    <ContentBox id='about'>
+      <h3 className='mb-4 text-white text-2xl font-semibold'>{headline}</h3>
+      <h4 className='mb-4 text-neutral-100 font-bold text-xl tracking-wide leading-normal'>
+        {subheadline}
+      </h4>
+      {!!paragraphs &&
+        Object.keys(paragraphs).map(item => {
+          return (
+            <p key={item} className='mb-4 leading-relaxed text-neutral-300'>
+              {paragraphs[item]}
+            </p>
+          );
+        })}
+    </ContentBox>
+  );
 };
 
 export default About;
--- components/Resume/index.tsx
+++ components/Resume/index.tsx
@@ -5,19 +5,21 @@ import React from 'react';
 import Card from '@/components/Card';
 
 const Resume: React.FC = () => {
-	return (
-		<Card height='h-fit'>
-			<p className='mx-4 mb-4 text-white text-lg font-semibold'>Downloadables</p>
-			<a
-				className='block w-fit mx-4 px-4 py-2 text-neutral-800 font-medium bg-neutral-200 rounded-3xl hover:bg-neutral-100'
-				href={process.env.NEXT_PUBLIC_RESUME}
-				target='_blank'
-				referrerPolicy='no-referrer'
-			>
-				Resume
-			</a>
-		</Card>
-	);
+  return (
+    <Card height='h-fit'>
+      <p className='mx-4 mb-4 text-white text-lg font-semibold'>
+        View or Download
+      </p>
+      <a
+        className='block w-fit mx-4 px-4 py-2 text-neutral-800 font-medium bg-neutral-200 rounded-3xl hover:bg-neutral-100'
+        href={process.env.NEXT_PUBLIC_RESUME}
+        target='_blank'
+        referrerPolicy='no-referrer'
+      >
+        Resume
+      </a>
+    </Card>
+  );
 };
 
 export default Resume;
