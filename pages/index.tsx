c8cdad79623cb786d73092d9308366cd36bddba9
Almer Tampus <almerflorestampus@gmail.com>
2023-10-14 22:16:45 +0800

    Set revalidate to 60 seconds

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,69 +11,72 @@ import Experience from '@/components/Experience';
 import Work from '@/components/Work';
 import Personal from '@/components/Personal';
 
-const Home: React.FC<HomeProps> = (props) => {
-	const { staticdata } = props;
+const Home: React.FC<HomeProps> = props => {
+  const { staticdata } = props;
 
-	// query handler
-	const queryHandler = async () => {
-		// api
-		const response = await fetch('api/v1/firebase/all');
-		const data = await response.json();
+  // query handler
+  const queryHandler = async () => {
+    // api
+    const response = await fetch('api/v1/firebase/all');
+    const data = await response.json();
 
-		return data;
-	};
+    return data;
+  };
 
-	// query
-	const { data } = useQuery({
-		queryKey: ['data'],
-		queryFn: queryHandler,
-		initialData: staticdata,
-		staleTime: 1000 * 60 * 1, // 1 minute
-		refetchInterval: 1000 * 60 * 1, // 1 minute
-		refetchIntervalInBackground: true,
-	});
+  // query
+  const { data } = useQuery({
+    queryKey: ['data'],
+    queryFn: queryHandler,
+    initialData: staticdata,
+    staleTime: 1000 * 60 * 1, // 1 minute
+    refetchInterval: 1000 * 60 * 1, // 1 minute
+    refetchIntervalInBackground: true,
+  });
 
-	return (
-		<Fragment>
-			<Head title='Almer Tampus' />
-			<Layout
-				renderHero={() => {
-					return <Hero {...data?.hero} />;
-				}}
-				renderContent={() => {
-					return (
-						<Fragment>
-							<About {...data?.biography} />
-							<Experience data={data?.experience} />
-							<Work data={data?.work} />
-							<Personal data={data?.personal} />
-						</Fragment>
-					);
-				}}
-			/>
-		</Fragment>
-	);
+  return (
+    <Fragment>
+      <Head title='Almer Tampus' />
+      <Layout
+        renderHero={() => {
+          return <Hero {...data?.hero} />;
+        }}
+        renderContent={() => {
+          return (
+            <Fragment>
+              <About {...data?.biography} />
+              <Experience data={data?.experience} />
+              <Work data={data?.work} />
+              <Personal data={data?.personal} />
+            </Fragment>
+          );
+        }}
+      />
+    </Fragment>
+  );
 };
 
 export async function getStaticProps() {
-	try {
-		const response = await fetch(`${process.env.NEXT_PUBLIC_URL}/api/v1/firebase/all`);
-		const staticdata = await response.json();
+  try {
+    const response = await fetch(
+      `${process.env.NEXT_PUBLIC_URL}/api/v1/firebase/all`,
+    );
+    const staticdata = await response.json();
 
-		return {
-			props: {
-				staticdata,
-			},
-		};
-	} catch (error) {
-		console.log(error);
+    return {
+      props: {
+        staticdata,
+      },
+      revalidate: 60,
+    };
+  } catch (error) {
+    console.log(error);
 
-		return {
-			props: {
-				staticdata: null,
-			},
-		};
-	}
+    return {
+      props: {
+        staticdata: null,
+      },
+    };
+  }
 }
 
 export default Home;
