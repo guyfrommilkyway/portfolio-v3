7143b6cb36ac404ffce122759c1c9016538d597b
Almer Tampus <almerflorestampus@gmail.com>
2023-10-16 07:13:41 +0800

    Update types

--- types/projects.d.ts
+++ types/projects.d.ts
@@ -5,7 +5,12 @@ interface ProjectCardProps {
   image: string;
   name: string;
   link: string;
+  company: string;
+  hostingProvider: string;
+  teamSize: string;
+  codebase: string;
   status: string;
   description: string;
+  disclaimer: string;
   technologies: any;
 }
