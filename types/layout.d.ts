9539abfaa8736b2902977e716c0bdd52e357a6f0
Almer Tampus <almerflorestampus@gmail.com>
2023-10-17 05:35:25 +0800

    Update types

--- types/layout.d.ts
+++ types/layout.d.ts
@@ -1,23 +1,26 @@
 /// <reference path="types.ts" />
 
-type Component = (() => JSX.Element | JSX.Element[]) | JSX.Element | JSX.Element[] | MyReactNode;
+type Component =
+  | (() => JSX.Element | JSX.Element[])
+  | JSX.Element
+  | JSX.Element[]
+  | MyReactNode;
 
 interface ChildrenProps {
-	children: Component;
+  children: Component;
 }
 
 interface NavItemProps {
-	name: string;
-	onScroll: () => void;
+  name: string;
+  link: string;
 }
 
 interface LayoutProps {
-	renderHero: Component;
-	renderContent: Component;
-	// renderNews: Component;
+  hero: any;
+  children: Component;
 }
 
 interface ContentBoxProps {
-	id: string;
-	children: Component;
+  id: string;
+  children: Component;
 }
