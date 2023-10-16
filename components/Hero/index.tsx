b25eea50aa917ba763b30654391f7c6142007edd
Almer Tampus <almerflorestampus@gmail.com>
2023-10-17 07:11:07 +0800

    Update padding

--- components/Hero/index.tsx
+++ components/Hero/index.tsx
@@ -12,7 +12,7 @@ const Hero: React.FC<HeroProps> = props => {
   const { headline, title, description } = props;
 
   return (
-    <aside className='relative lg:sticky lg:top-[80px] flex flex-col w-full md:max-w-[320px] h-fit px-4 pt-4 pb-10 border-b md:border-none border-neutral-900'>
+    <aside className='relative lg:sticky lg:top-[80px] flex flex-col w-full md:max-w-[320px] h-fit px-4 pt-4 pb-10 md:px-8 lg:px-4 border-b md:border-none border-neutral-900'>
       <Photo />
       <h1 className='mb-2 text-white font-bold text-3xl leading-tighter tracking-wider select-none'>
         {headline}
