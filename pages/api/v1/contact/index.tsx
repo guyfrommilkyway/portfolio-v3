dfeb6d2b1d9065db064897d43240a07ed8ea1fb4
Almer Tampus <almerflorestampus@gmail.com>
2023-10-19 19:03:25 +0800

    Add rate limiting to 1 request per minute

--- pages/api/v1/contact/index.tsx
+++ pages/api/v1/contact/index.tsx
@@ -1,14 +1,38 @@
 // packages
 import { NextApiRequest, NextApiResponse } from 'next';
+import requestIp from 'request-ip';
+import Redis from 'ioredis';
 const sgMail = require('@sendgrid/mail');
 
+// utils
+import rateLimiter from '@/utils/rate-limiter';
+
 sgMail.setApiKey(process.env.NEXT_PUBLIC_SENDGRID_API_KEY);
+
+const client = new Redis({
+  host: process.env.NEXT_PUBLIC_REDIS_URL!,
+  password: process.env.NEXT_PUBLIC_REDIS_TOKEN!,
+  port: +process.env.NEXT_PUBLIC_REDIS_PORT!,
+});
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse,
 ) {
   try {
+    const identifier = requestIp.getClientIp(req);
+    const result = await rateLimiter(client, identifier!, 1, 60);
+    res.setHeader('X-RateLimit-Limit', result.limit);
+    res.setHeader('X-RateLimit-Remaining', result.remaining);
+
+    if (!result.success) {
+      res.status(429).json({
+        status: 429,
+        message: 'Too many requests in 1 minute. Please try again later.',
+      });
+      return;
+    }
+
     const response = await sgMail.send({
       to: process.env.NEXT_PUBLIC_SENDGRID_TO_EMAIL,
       from: process.env.NEXT_PUBLIC_SENDGRID_FROM_EMAIL,
@@ -19,6 +43,7 @@ export default async function handler(
 
     res.status(200).json(response);
   } catch (error: any) {
-    res.status(error.code).json(error);
+    console.log('Error');
+    res.status(error.code).json(error.response);
   }
 }
