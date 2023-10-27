320a32e7b54ae80f0751be800b7050d326881b77
Almer Tampus <almerflorestampus@gmail.com>
2023-10-27 16:56:04 +0800

    Update types

--- types/form.d.ts
+++ types/form.d.ts
@@ -1,3 +1,25 @@
+/// <reference path="types.ts" />
+
+interface FormLabelProps {
+  id: string;
+  text: string;
+}
+
+interface FormInputProps {
+  id: string;
+  label: string;
+  type?: string;
+  isInvalid?: boolean;
+  errMsg?: string;
+  register: MyUseFormRegisterReturn;
+}
+
 interface FormErrorMessageProps {
   message: string | undefined;
+}
+
+interface FormButtonProps {
+  isSubmitting: boolean;
+  isRateLimited: boolean;
+  errMsg: string;
 }
