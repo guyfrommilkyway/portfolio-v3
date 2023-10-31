aef7cd2b2a62162be6879089b5a94f19663f5b5d
Almer Tampus <almerflorestampus@gmail.com>
2023-10-31 20:01:26 +0800

    Minor UI enhancements

--- components/UI/SectionUI/index.tsx
+++ components/UI/SectionUI/index.tsx
@@ -5,7 +5,7 @@ const SectionUI: React.FC<SectionUIProps> = props => {
   const { children, headline } = props;
 
   return (
-    <section className='relative w-full px-4 py-8 md:px-8 lg:px-10 border-b border-neutral-900'>
+    <section className='relative w-full px-4 py-8 md:px-8 lg:px-10'>
       <h1 className='mb-4 text-white text-xl font-semibold select-none'>
         {headline}
       </h1>
