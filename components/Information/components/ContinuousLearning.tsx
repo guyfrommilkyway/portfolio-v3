317dbd3a3bd5bebc223f2fe9b28148bd256d562c
Almer Tampus <almerflorestampus@gmail.com>
2023-12-28 23:45:18 +0800

    Remove unnecessary props

--- components/Information/components/ContinuousLearning.tsx
+++ components/Information/components/ContinuousLearning.tsx
@@ -6,7 +6,7 @@ import CardHeader from './CardHeader';
 import CardDuration from './CardDuration';
 
 const ContinuousLearning: React.FC<ContinuousLearningCardProps> = props => {
-  const { name, duration, courses } = props;
+  const { name, duration } = props;
 
   return (
     <div className=''>
