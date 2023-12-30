58c43e187d3f47db0f46da9c4de7513b2d35b826
Almer Tampus <almerflorestampus@gmail.com>
2023-12-30 08:11:14 +0800

    Set username to sr-only

--- components/Hero/components/Social.tsx
+++ components/Hero/components/Social.tsx
@@ -6,13 +6,13 @@ const Link: React.FC<SocialProps> = props => {
 
   return (
     <a
-      className='flex items-center gap-3 text-neutral-400 font-medium underline leading-none transition-colors ease-in-out delay-100 hover:text-white'
+      className='flex items-center gap-4 text-neutral-400 font-medium leading-none transition-colors ease-in-out delay-100 hover:text-white'
       href={link}
       target='_blank'
       rel='noopener noreferrer'
     >
       {icon}
-      <span className='select-none'>{username}</span>
+      <span className='sr-only'>{username}</span>
     </a>
   );
 };
