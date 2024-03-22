dd09290b751916ea7d3e592be1834a7c80656210
Almer Tampus <almerflorestampus@gmail.com>
2024-03-22 17:39:05 +0800

    feat: organize data

--- hooks/useToggle.ts
+++ hooks/useToggle.ts
@@ -2,12 +2,12 @@
 import { useState } from 'react';
 
 const useToggle = () => {
-	// state
-	const [toggle, setToggle] = useState<boolean>(false);
+  const [toggle, setToggle] = useState<boolean>(false);
 
-	const toggleHandler = (param?: boolean) => setToggle((prev) => (param ? param : !prev));
+  const toggleHandler = (param?: boolean) =>
+    setToggle(prev => (param ? param : !prev));
 
-	return { toggle, toggleHandler };
+  return { toggle, toggleHandler };
 };
 
 export default useToggle;
