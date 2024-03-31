9246b63035452d00fba537455d0c73bee5de7560
Almer Tampus <almerflorestampus@gmail.com>
2024-04-01 02:14:22 +0800

    feat: minor refactoring + clean code

--- components/common/LinkExternal/index.tsx
+++ components/common/LinkExternal/index.tsx
@@ -2,21 +2,25 @@
 import React from 'react';
 
 // assets
-import ArrowUpSVG from '@/assets/svg/arrow.svg';
+import { FaArrowUpRightFromSquare } from 'react-icons/fa6';
 
-const LinkExternal: React.FC<LinkExternalProps> = props => {
-  const { href, title, name } = props;
+interface PLinkExternal {
+  title: string;
+  href: string;
+  name?: string;
+}
 
+const LinkExternal: React.FC<PLinkExternal> = ({ title, name, href }) => {
   return (
     <a
-      className='flex gap-1 w-fit text-neutral-300 text-sm underline hover:text-white transition-colors ease-in-out delay-100 select-none'
+      className='flex gap-1 w-fit text-neutral-400 underline hover:text-white transition-colors ease-in-out delay-100 select-none'
       href={href}
       title={title}
       target='_blank'
       rel='noopener noreferrer'
     >
-      {name ? name : 'View Website'}
-      <ArrowUpSVG width={10} height={10} />
+      {name ?? 'View Website'}
+      <FaArrowUpRightFromSquare className='inline ml-1 mt-1' size={10} />
     </a>
   );
 };
