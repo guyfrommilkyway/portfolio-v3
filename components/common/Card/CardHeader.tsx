edda466ed774145902906de88cf62880682f6471
Almer Tampus <almerflorestampus@gmail.com>
2024-04-05 10:58:53 +0800

    style: minor ui enhancements

--- components/common/Card/CardHeader.tsx
+++ components/common/Card/CardHeader.tsx
@@ -8,7 +8,7 @@ interface PCardHeader {
 
 const CardHeader: React.FC<PCardHeader> = ({ title, href }) => {
   return (
-    <h1 className='text-neutral-300 font-semibold leading-snug tracking-wide hover:text-white transition-colors ease-in-out delay-100 select-none'>
+    <h1 className='text-neutral-300 font-semibold tracking-tighter hover:text-white transition-colors ease-in-out delay-100 select-none'>
       {href ? (
         <a href={href} title={title} target='_blank' rel='noopener noreferrer'>
           {title}
--- components/common/Layout/components/SideContent.tsx
+++ components/common/Layout/components/SideContent.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 
 const SideContent: React.FC<PChildren> = ({ children }) => {
   return (
-    <aside className='hidden xl:flex flex-col gap-4 w-full max-w-[340px] h-fit my-4 px-4'>
+    <aside className='hidden xl:flex flex-col gap-4 w-full max-w-[320px] h-fit my-4 px-4'>
       {children}
     </aside>
   );
