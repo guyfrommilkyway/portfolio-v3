be075f5340647786c207349e3464a7532b863561
Almer Tampus <almerflorestampus@gmail.com>
2025-07-30 02:39:19 +0800

    chore: update logo text

--- src/components/common/Logo/index.tsx
+++ src/components/common/Logo/index.tsx
@@ -4,10 +4,10 @@ import React from 'react';
 const Logo: React.FC = () => {
     return (
         <Link
-            className='text-white hover:text-white hover:underline transition-colors ease-in-out delay-100 select-none'
+            className='text-white font-semibold tracking-widest select-none cursor-pointer transition duration-200 ease-in-out hover:underline'
             href='/'
         >
-            almertampus
+            ALMER
         </Link>
     );
 };
